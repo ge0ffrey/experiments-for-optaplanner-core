7279af8c73cc96ac38349b289285e6a1a54e2c83
ge0ffrey <ge0ffrey.spam@gmail.com>
2010-12-17 12:44:06 +0100

    dog name

---
+++ src/main/java/be/ge0ffrey/mysandbox/Dog.java
@@ -0,0 +1,17 @@
+package be.ge0ffrey.mysandbox;
+
+/**
+ * @author Geoffrey De Smet
+ */
+public class Dog {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
