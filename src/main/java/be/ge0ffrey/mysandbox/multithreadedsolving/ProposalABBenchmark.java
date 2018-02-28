41a26cb9063e7e78a3dd7127ba67f848b0a9555d
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2018-02-28 04:40:02 +0100

    Fix line ending issue

--- src/main/java/be/ge0ffrey/mysandbox/multithreadedsolving/ProposalABBenchmark.java
+++ src/main/java/be/ge0ffrey/mysandbox/multithreadedsolving/ProposalABBenchmark.java
@@ -3,7 +3,7 @@ package be.ge0ffrey.mysandbox.multithreadedsolving;
 public class ProposalABBenchmark {
 
     private static final int TIME_IN_MS = 120_000;
-    private static final int[] THREAD_COUNTS = {1, 2, 4, 8, 16, 18};
+    private static final int[] THREAD_COUNTS = {1, 2, 4, 8, 16, 32, 64};
     private static final int[][] LOOP_SIZES = {
             {3_000, 3_000},
             {30_000, 30_000},
@@ -13,24 +13,19 @@ public class ProposalABBenchmark {
             {30_000, 3_000_000}};
 
     public static void main(String[] args) {
-        StringBuilder resultA = new StringBuilder("threadCount,loopSizeMin,loopSizeMax,speed");
-        StringBuilder resultB = new StringBuilder("threadCount,loopSizeMin,loopSizeMax,speed");
+        StringBuilder result = new StringBuilder("threadCount,loopSizeMin,loopSizeMax,speedA, speed B");
         for (int threadCount : THREAD_COUNTS) {
             for (int[] loopSize : LOOP_SIZES) {
                 Calculator calculator = new Calculator(loopSize[0], loopSize[1]);
                 long speedA = new ProposalA(threadCount, TIME_IN_MS, calculator).runAndReturnSpeed();
-                resultA.append(threadCount).append(",").append(loopSize[0]).append(",").append(loopSize[1]).append(",").append(speedA);
                 long speedB = new ProposalB(threadCount, TIME_IN_MS, calculator).runAndReturnSpeed();
-                resultB.append(threadCount).append(",").append(loopSize[0]).append(",").append(loopSize[1]).append(",").append(speedB);
+                result.append(threadCount).append(",").append(loopSize[0]).append(",").append(loopSize[1]).append(",").append(speedA).append(",").append(speedB).append("\n");
             }
         }
         System.out.println("");
-        System.out.println("Result A");
-        System.out.println("========");
-        System.out.println(resultA);
-        System.out.println("Result B");
-        System.out.println("========");
-        System.out.println(resultB);
+        System.out.println("Result");
+        System.out.println("======");
+        System.out.println(result);
     }
 
 }
